cac668061772ce449e679163173c30daae2fd817
MatteoSala97 <matteo111997@gmail.com>
2024-01-16 17:03:42 +0100

    feat: campo minato completed (no bonus)

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,19 @@ function generateGrid(){
     for (let i = 1; i <= 100; i++){
         //creates an empty div
         let box = document.createElement("div")
+
         //adds the class to the div
         box.classList.add("box")
+    
+        //numerates the cells 
+        box.innerText = i
+
         //creates the grid
         gridHtml.append(box)
     
         box.addEventListener("click", function(){
             this.classList.toggle("active")
+            console.log(i)
         })
     }
 }
@@ -23,5 +29,3 @@ btnHtml.addEventListener("click",function(){
     generateGrid()
     gridHtml.classList.toggle("none")
 })
-
-
