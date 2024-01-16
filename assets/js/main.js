215225c0529288bffa2ebbfb7d7957f9b64ce482
MatteoSala97 <matteo111997@gmail.com>
2024-01-16 18:13:26 +0100

    feat: bonus done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,28 @@
 const gridHtml = document.getElementById("grid")
 const btnHtml = document.getElementById("btn")
 
+let divisor = 10
+
 function generateGrid(){
 
     gridHtml.innerHTML =""
 
-    for (let i = 1; i <= 100; i++){
+    for (let i = 1; i <= divisor * divisor; i++){
         //creates an empty div
         let box = document.createElement("div")
 
+        if (selectedDifficulty === "Easy"){
+            divisor = 10
+        } else if ( selectedDifficulty === "Medium"){
+            divisor = 9
+        } else {
+            divisor = 7
+        }
+
         //adds the class to the div
         box.classList.add("box")
-    
+        box.style.width = `calc( 100% / ${divisor})`
+
         //numerates the cells 
         box.innerText = i
 
@@ -25,7 +36,23 @@ function generateGrid(){
     }
 }
 
-btnHtml.addEventListener("click",function(){
-    generateGrid()
-    gridHtml.classList.toggle("none")
+const radioButtons = document.querySelectorAll('input[name="difficulty"]')
+
+let selectedDifficulty 
+
+btnHtml.addEventListener("click", function () {
+
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+
+            selectedDifficulty = radioButton.value
+            console.log(selectedDifficulty)
+
+            generateGrid()
+            gridHtml.classList.remove("none")
+
+            break
+        }
+    }
 })
+
