69e6543b0118e943e22e583bfae337dd33de09a2
MatteoSala97 <matteo111997@gmail.com>
2024-01-16 16:33:55 +0100

    feat: grid toggle button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,27 @@
 const gridHtml = document.getElementById("grid")
 const btnHtml = document.getElementById("btn")
 
-for (let i = 1; i <= 100; i++){
-    //creates an empty div
-    let box = document.createElement("div")
-    //adds the class to the div
-    box.classList.add("box")
-    //creates the grid
-    gridHtml.append(box)
+function generateGrid(){
 
-    box.addEventListener("click", function(){
-        this.classList.toggle("active")
-    })
+    gridHtml.innerHTML =""
 
+    for (let i = 1; i <= 100; i++){
+        //creates an empty div
+        let box = document.createElement("div")
+        //adds the class to the div
+        box.classList.add("box")
+        //creates the grid
+        gridHtml.append(box)
+    
+        box.addEventListener("click", function(){
+            this.classList.toggle("active")
+        })
+    }
 }
 
 btnHtml.addEventListener("click",function(){
-    gridHtml.classList.remove("none")
+    generateGrid()
+    gridHtml.classList.toggle("none")
 })
+
+
